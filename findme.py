70767be0e53eacb3a6e21e6f7baea35c79c1f46d
Walt Deyzel <waltdeyzel3141@gmail.com>
2020-06-22 20:22:28 +0200

    Sort all relavent links on page into a list. Pick a random one and navigate to that wiki site.

--- findme.py
+++ findme.py
@@ -1,27 +1,43 @@
 from bs4 import BeautifulSoup
 import requests 
 import webbrowser
+from random import randint
+import time
 
 find = "Handford"#str(input())
 found = False
-#while(found == False):
-site = "https://en.wikipedia.org/wiki/Where%27s_Wally%3F"
+link_count = 5
 
-page = requests.get(site)
+wiki = "/wiki/Where%27s_Wally%3F"
+basis = "https://en.wikipedia.org"
+while(link_count > 0):
+    site = basis + wiki
 
-soup = BeautifulSoup(page.content, 'html.parser')
-texts = soup.findAll(text=True)
-links = soup.find_all('a')
+    page = requests.get(site)
 
-#webbrowser.open(site)
-for text in texts:
-    for word in text.split():
-        if word == find:
-            print('found')
-            found = True
+    soup = BeautifulSoup(page.content, 'html.parser')
+    texts = soup.findAll(text=True)
+    links = soup.find_all('a', href=True)
+
+    #webbrowser.open(site)
+    for text in texts:
+        for word in text.split():
+            if word == find:
+                print('found', word)
+                found = True
+                break
+        if(found):
             break
-    if(found):
-        break
 
-if found == False:
-    print('Nope')
+    random_links = []
+    for link in links:
+        if(str(link['href'])[0:6] == '/wiki/'):
+            if(link['href'][6:14] !='Category' and link['href'][6:12] !='Where'):
+                random_links.append(link['href'])
+        
+    index = randint(0, len(random_links))
+    wiki = basis+random_links[index]
+    print(wiki)
+    webbrowser.open(wiki)
+    time.sleep(3)
+    link_count -= 1
