a3bb9c3f2cb108ad2dba0fdf826002dd52708f0f
Walt Deyzel <waltdeyzel3141@gmail.com>
2020-06-23 10:16:10 +0200

    Solved the requests error. The program will now go to a new page after selecting a random link and then repeating the process. It will stop when it finds the word

--- findme.py
+++ findme.py
@@ -4,40 +4,67 @@ import webbrowser
 from random import randint
 import time
 
-find = "Handford"#str(input())
+find = str(input(" Find ? :"))
 found = False
-link_count = 5
+link_count = 0
+limit = 200
 
 wiki = "/wiki/Where%27s_Wally%3F"
 basis = "https://en.wikipedia.org"
-while(link_count > 0):
-    site = basis + wiki
+def cleanWord(word):
+    word = word.lower()
+    word = word.replace('%', '')
+    word = word.replace(',', '')
+    word = word.replace('.', '')
+    word = word.replace('?', '')
+    word = word.replace(':', '')
+    word = word.replace('(', '')
+    word = word.replace(')', '')
+    return word
 
+while(link_count < limit and found == False):
+  
+    site = basis + wiki
+    time.sleep(1)
+    #REQUEST PAGE
     page = requests.get(site)
 
     soup = BeautifulSoup(page.content, 'html.parser')
-    texts = soup.findAll(text=True)
+    #FIND ALL TEXT ON SCREEN
+    texts_on_screen = soup.findAll(text=True)
+    #FINE ALL LINKS ON SCREEN.
     links = soup.find_all('a', href=True)
-
-    #webbrowser.open(site)
-    for text in texts:
-        for word in text.split():
-            if word == find:
+    
+    for sentence in texts_on_screen:
+        for word in sentence.split():
+            if(cleanWord(word) == find):
                 print('found', word)
                 found = True
+                time.sleep(1)
+                webbrowser.open(site)
+                print(link_count, 'Links later...')
                 break
         if(found):
             break
+    if found:
+        break
 
     random_links = []
+    #FILTER LINKS TO BE JUST WIKI PAGES
     for link in links:
         if(str(link['href'])[0:6] == '/wiki/'):
-            if(link['href'][6:14] !='Category' and link['href'][6:12] !='Where'):
-                random_links.append(link['href'])
+            if(link['href'][6:14] !='Category' and link['href'][6:11] !='Where' and link['href'][6:10] != 'Help' and link['href'][6:15] != 'Wikipedia'):
+                random_links.append(str(link['href']))
         
+    #SELECT RANDOM LINK IN RANDOM LINKS LIST
     index = randint(0, len(random_links))
-    wiki = basis+random_links[index]
-    print(wiki)
-    webbrowser.open(wiki)
-    time.sleep(3)
-    link_count -= 1
+    #THAT LINK IS THE NEW WIKIPEDIA PAGE
+    wiki = random_links[index]
+    #DISPLAY WIKI AND NUMBER OF LINKS ON PAGE
+    print(wiki, len(random_links))
+    link_count += 1
+
+if(found == False):
+    print('The word clearly does not exist')
+print('DONE')
+
